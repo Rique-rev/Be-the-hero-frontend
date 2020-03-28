7244cc8cfe20af7fc5f4e9ecec442f45a7d6d42f
Henrique Ribeiro Evangelista <henrique.rev@gmail.com>
2020-03-28 19:09:32 -0300

    funcionalidade login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -5,9 +5,28 @@ import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 import { FiLogIn } from 'react-icons/fi'
 
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
 
 export default function Logon() {
+    const history = useHistory()
+    const [id, setId] = useState('')
+
+    async function handleLogin(e) {
+        e.preventDefault()
+
+        try {
+            const response = await api.post('/sessions', { id })
+            history.push('/profile')
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.message.name)
+        }
+        catch (err) {
+            console.log(err)
+            alert('Falha no login, tente novamente!')
+        }
+    }
 
     return (
         <div className="logon-container">
@@ -15,16 +34,20 @@ export default function Logon() {
             <section className="form">
                 <img src={logoImg} alt="Be the Hero" />
 
-                <form >
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link to="/register" className="back-link">Não tenho cadastro</Link>
-                    <FiLogIn className="iconLogin" size={ 16 } color="#E02041" />
+                    <FiLogIn className="iconLogin" size={16} color="#E02041" />
                 </form>
-                
+
             </section>
 
             <img src={heroesImg} alt="Heroes" />
